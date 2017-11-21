02fc747b5f1fdd225abb9ec6c2b734349676add6
Mitch <mitchroutt@gmail.com>
2017-11-20 21:33:22 -0600

    First stab at implementing a 52 card shuffled deck.

---
+++ GameEngine.Tests/DeckShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class DeckShould
+    {
+        [Fact]
+        public void Draw52Cards()
+        {
+            int count = 0;
+            var deck = new Deck();
+            var drawnCards = new List<Card>();
+
+            while (count < 52)
+            {
+                var card = deck.DrawCard();
+                Assert.NotNull(card);
+                Assert.False(drawnCards.Contains(card));
+                drawnCards.Add(card);
+                Assert.True(drawnCards.Contains(card));
+                count++;
+            }
+
+            Assert.Equal(52, count);
+        }
+    }
+}
---
+++ GameEngine/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine
+{
+    public class Card
+    {
+        public char Suit { get; private set; }
+        public int Value { get; private set; }
+
+        public Card(char suit, int value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+}
